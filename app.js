ead23058ab6c53bb81a012874a74556dd16348cc
carloseduardo280689-dev <carloseduardo280689@gmail.com>
2025-11-05 01:03:38 -0300

    creating rotes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(8081,function(){
-    console.log("Server online localhost:8081 ok");
-})
+app.get("/", function (req, res) {
+  res.send("Seja bem vindo ao nosso site!");
+});
+
+app.get("/artigos/:id", function (req, res) {
+  res.send("Página de artigos");
+});
+
+app.get("/contato", function (req, res) {
+  res.send("Página de contato");
+});
+
+app.listen(8081, function () {
+  console.log("Server online localhost:8081 ok");
+});
